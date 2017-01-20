aa58b0dfe15c071168a903ccbc9444ee10a58a42
emilijus789 <emilis.masalskis@gmail.com>
2017-01-20 21:46:46 +0200

    First website upload

---
+++ jquery.js
@@ -0,0 +1,163 @@
+$(document).ready(function(){
+    
+    
+    $("#nd1").fadeTo('fast', 0.7);
+    $("#nd1").mouseenter(function(){
+       $("#nd1").fadeTo('fast', 1);
+    }); 
+    $("#nd1").mouseleave(function(){
+       $("#nd1").fadeTo('fast', 0.7);
+    });
+    
+    
+      $("#nd2").fadeTo('fast', 0.7);
+    $("#nd2").mouseenter(function(){
+       $("#nd2").fadeTo('fast', 1);
+    }); 
+    $("#nd2").mouseleave(function(){
+       $("#nd2").fadeTo('fast', 0.7);
+    });
+    
+      $("#nd3").fadeTo('fast', 0.7);
+    $("#nd3").mouseenter(function(){
+       $("#nd3").fadeTo('fast', 1);
+    }); 
+    $("#nd3").mouseleave(function(){
+       $("#nd3").fadeTo('fast', 0.7);
+    });
+    
+      $("#nd4").fadeTo('fast', 0.7);
+    $("#nd4").mouseenter(function(){
+       $("#nd4").fadeTo('fast', 1);
+    }); 
+    $("#nd4").mouseleave(function(){
+       $("#nd4").fadeTo('fast', 0.7);
+    });
+    
+    $("#nd5").fadeTo('fast', 0.7);
+    $("#nd5").mouseenter(function(){
+       $("#nd5").fadeTo('fast', 1);
+    }); 
+    $("#nd5").mouseleave(function(){
+       $("#nd5").fadeTo('fast', 0.7);
+    });
+    
+    $("#nd6").fadeTo('fast', 0.7);
+    $("#nd6").mouseenter(function(){
+       $("#nd6").fadeTo('fast', 1);
+    }); 
+    $("#nd6").mouseleave(function(){
+       $("#nd6").fadeTo('fast', 0.7);
+    });
+    
+    $("#nd7").fadeTo('fast', 0.7);
+    $("#nd7").mouseenter(function(){
+       $("#nd7").fadeTo('fast', 1);
+    }); 
+    $("#nd7").mouseleave(function(){
+       $("#nd7").fadeTo('fast', 0.7);
+    });
+    
+    $("#nd8").fadeTo('fast', 0.7);
+    $("#nd8").mouseenter(function(){
+       $("#nd8").fadeTo('fast', 1);
+    }); 
+    $("#nd8").mouseleave(function(){
+       $("#nd8").fadeTo('fast', 0.7);
+    });
+    
+    $("#nd9").fadeTo('fast', 0.7);
+    $("#nd9").mouseenter(function(){
+       $("#nd9").fadeTo('fast', 1);
+    }); 
+    $("#nd9").mouseleave(function(){
+       $("#nd9").fadeTo('fast', 0.7);
+    });
+    
+    $("#nd10").fadeTo('fast', 0.7);
+    $("#nd10").mouseenter(function(){
+       $("#nd10").fadeTo('fast', 1);
+    }); 
+    $("#nd10").mouseleave(function(){
+       $("#nd10").fadeTo('fast', 0.7);
+    });
+    
+    
+    
+     $("#m1").css("color", "gray");
+    $("#m1").mouseenter(function(){
+       $("#m1").css("color", "white");
+    }); 
+    $("#m1").mouseleave(function(){
+       $("#m1").css("color", "gray");
+    });
+    
+    $("#m2").css("color", "gray");
+    $("#m2").mouseenter(function(){
+       $("#m2").css("color", "white");
+    }); 
+    $("#m2").mouseleave(function(){
+       $("#m2").css("color", "gray");
+    });
+    
+    $("#m3").css("color", "gray");
+    $("#m3").mouseenter(function(){
+       $("#m3").css("color", "white");
+    }); 
+    $("#m3").mouseleave(function(){
+       $("#m3").css("color", "gray");
+    });
+    
+    
+    
+    $("#apie1").hide();
+    $("#nd1").click(function(){
+    $("#apie1").fadeToggle();
+    });
+    
+     $("#apie2").hide();
+    $("#nd2").click(function(){
+    $("#apie2").fadeToggle();
+    });
+    
+     $("#apie3").hide();
+    $("#nd3").click(function(){
+    $("#apie3").fadeToggle();
+    });
+    
+     $("#apie4").hide();
+    $("#nd4").click(function(){
+    $("#apie4").fadeToggle();
+    });
+    
+    $("#apie5").hide();
+    $("#nd5").click(function(){
+    $("#apie5").fadeToggle();
+    });
+    
+    $("#apie6").hide();
+    $("#nd6").click(function(){
+    $("#apie6").fadeToggle();
+    });
+    
+    $("#apie7").hide();
+    $("#nd7").click(function(){
+    $("#apie7").fadeToggle();
+    });
+    
+    $("#apie8").hide();
+    $("#nd8").click(function(){
+    $("#apie8").fadeToggle();
+    });
+    
+    $("#apie9").hide();
+    $("#nd9").click(function(){
+    $("#apie9").fadeToggle();
+    });
+    
+    $("#apie10").hide();
+    $("#nd10").click(function(){
+    $("#apie10").fadeToggle();
+    });
+    
+});
